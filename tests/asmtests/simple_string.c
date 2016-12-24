16aff02aef8110b76ae21e8c55d4e4ca8aaa35c3
Filip <filipbenna@gmail.com>
2016-12-24 23:14:45 +0100

    simple_string test changed

--- tests/asmtests/simple_string.c
+++ tests/asmtests/simple_string.c
@@ -5,5 +5,11 @@ int main(void) {
     print_string(a);
     print_string(b);
 
+    string s, mod;
+    s = "Hello\tworld!";
+    mod = set_at(s, 5, 'n');
+    print_string(s);
+    print_string(mod);
+
     return 42;
 }
